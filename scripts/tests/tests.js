8dc72bccc3d92f70eca18863fa9133b4e882707a
Radek Novak <radek.novak.cze@gmail.com>
2014-05-23 09:10:47 +0200

    zalozeni testovaci vetve

---
+++ scripts/tests/tests.js
@@ -0,0 +1,19 @@
+var el_testImages = document.querySelectorAll('.test-images img'),
+	testImages = [];
+
+/*for (var i = 0, ii = i < el_testImages.length; i < ii; i++) {
+	testImages[i] = new Image();
+	testImages[i].onload = function () {
+		
+	}
+	testImages[i].src = this;
+}*/
+//var a = new Asciizer(this, getGridWidth());
+
+
+test( "hello test", function() {
+  ok( 1 == "1", "Passed!" );
+});
+test( "set grid dimensions", function() {
+  ok( 1 == "1", "Passed!" );
+});
