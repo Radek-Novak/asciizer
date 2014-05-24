f0bf75223488506693c89cdf5ddd0e9a7593d474
Radek Novak <radek.novak.cze@gmail.com>
2014-05-24 15:49:31 +0200

    first working tests

--- scripts/Asciizer.js
+++ scripts/Asciizer.js
@@ -1,11 +1,13 @@
 function Asciizer(imgObj, gridWidth) {
+    "use strict";
+
     var canvas = $('#atelier')[0],
         context = canvas.getContext('2d'),
         CHAR_RATIO = 7 / 13,
+        CHAR_W = 8,
+        CHAR_H = 16,
         img = imgObj,
-        ratio = img.width / img.height,
         gridHeight = null,
-        valueArray = [],
         min = Infinity,
         max = 0,
         sum = 0,
@@ -15,17 +17,48 @@ function Asciizer(imgObj, gridWidth) {
         charh = null,
         chars = " .-:*+=%#@".split("").reverse().join("");
 
-    canvas.width = img.width;
-    canvas.height = img.height;
+    this.image = imgObj;
+    this.valueArray = [];
+    this.pixelLightness = [];
+    this.grid_w = gridWidth;
+    this.grid_h;
 
-    this.setGridDimensions = function(w) {
-        gridWidth = w;
-        gridHeight = Math.floor(CHAR_RATIO * (gridWidth / ratio));
-        charw = Math.floor(canvas.width / gridWidth);
-        charh = Math.floor(canvas.height / gridHeight);
-    };
+    this.loadImage = function (i) {
+        this.image = i || imgObj;
+    }
 
-    this.calculate = function(w) {
+    this.setCanvasSize = function (w, h) {
+        var ratio = this.image.width / w;
+        
+        this.grid_w = w;
+        this.grid_h = h || (this.image.height / ratio);
+
+        canvas.width = w;
+        canvas.height = this.grid_h * 2;
+    }
+
+    this.draw = function (image2draw) {
+        image2draw = image2draw || this.image;
+        context.drawImage(image2draw, 0, 0, canvas.width, canvas.height);
+    }
+
+    this.readCanvas = function () {
+        this.valueArray = context.getImageData(0, 0, canvas.width, canvas.height).data;
+    }
+
+    this.calculate = function () {
+        for (var i = 0, ii = this.valueArray.length/4; i < ii; i++) {
+            var va = this.valueArray;
+            this.pixelLightness.push(
+                (
+                    va[4*i] + va[4*i + 1] + va[4*i + 2])
+                    * 
+                    (256 / (va[4*i+3] + 1)
+                )
+            );
+        }
+    }
+    /*this.calculate = function(w) {
         w = w || gridWidth;
         this.setGridDimensions(w);
         
@@ -42,13 +75,13 @@ function Asciizer(imgObj, gridWidth) {
         range = null;
         step = null;
         valueArray = [];
-    };
-    this.setGridDimensions(gridWidth);
+    };*/
+    //this.setGridDimensions(gridWidth);
 
 
-    this.calculate();
+    //this.calculate();
 
-    window.val = [charw, charh, gridWidth, gridHeight, canvas];
+    //window.val = [charw, charh, gridWidth, gridHeight, canvas];
 
 
     function getValues() {
@@ -147,17 +180,8 @@ function Asciizer(imgObj, gridWidth) {
         $('pre').text(text);
     }
 
-    function putRectangle(ctx, data, coords) {
-        ctx.putImageData(data, coords[0], coords[1]);
-    }
 
-    function paintGrid(ctx) {
-        for (var i = 0; i < gridHeight; i++) {
-            for (var j = 0; j < gridWidth; j++) {
-                context.strokeStyle = "rgb(20, 220,100)";
-                context.strokeRect(j * charw, i * charh, charw, charh);
-            }
-        }
-    }
-
+    (function startup () {
+        this.loadImage();
+    });
 }
--- scripts/tests/tests.js
+++ scripts/tests/tests.js
@@ -1,19 +1,105 @@
 var el_testImages = document.querySelectorAll('.test-images img'),
-	testImages = [];
 
-/*for (var i = 0, ii = i < el_testImages.length; i < ii; i++) {
-	testImages[i] = new Image();
-	testImages[i].onload = function () {
-		
-	}
-	testImages[i].src = this;
-}*/
-//var a = new Asciizer(this, getGridWidth());
+    testImages = {
+        white: $('[src$="white.jpg"]')[0],
+        black: $('[src$="black.jpg"]')[0],
+        diag: $('[src$="diag.jpg"]')[0]
+    },
+
+    asciizerObject = new Asciizer(el_testImages[0]),
+    canvas = $('#atelier')[0],
+    ctx = canvas.getContext('2d');
 
 
-test( "hello test", function() {
-  ok( 1 == "1", "Passed!" );
+
+test("Calculating and setting canvas dimensions", function() {
+    var testA = 20,
+        ascObj = new Asciizer(testImages.black),
+        cnvS, imgW, imgH, ratio;
+
+    function getCanvasSize(sel) {
+        var c = $(sel)[0] || $('#atelier')[0];
+
+        return {
+            w: c.width,
+            h: c.height
+        };
+    }
+    ascObj.setCanvasSize(testA);
+
+    imgW = ascObj.image.width;
+    imgH = ascObj.image.height;
+    ratio = imgW / testA;
+
+    // check
+    cnvS = getCanvasSize();
+    ok(cnvS.w === testA && cnvS.h === (imgH / ratio) * 2, "Canvas dimensions: " + cnvS.w +'x'+ cnvS.h);
+    ok(ascObj.grid_w === testA && ascObj.grid_h === (imgH / ratio), "Chargrid dimensions: " + ascObj.grid_w +'x'+ ascObj.grid_h);
+
 });
-test( "set grid dimensions", function() {
-  ok( 1 == "1", "Passed!" );
+
+
+test("Drawing to canvas", function() {
+    // set test values
+    var pretest,
+        result;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    pretest = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+
+    ctx.drawImage(el_testImages[19], 0, 0, canvas.width, canvas.height);
+    result = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+
+    // test
+
+    // check
+    notDeepEqual(pretest, result, "something was drawn");
+
+    //ok(result.w == a && result.h == b, "Passed!" );
+
+    // cleanup 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+test("Getting canvas raw values", function() {
+    "use strict";
+    // set test values
+    var oktest = true,
+        result,
+        counter = 0;
+
+    // test
+    asciizerObject.draw(testImages.black);
+    asciizerObject.readCanvas();
+    result = asciizerObject.valueArray;
+
+    for (var i = 0, ii = (result.length) / 4; i < ii; i++) {
+        var curtest = result[4 * i] === 0 &&
+            result[4 * i + 1] === 0 &&
+            result[4 * i + 2] === 0 &&
+            result[4 * i + 3] === 255;
+        oktest = oktest && curtest;
+        counter++;
+    }
+
+    ok(oktest, "reading drawn image");
+    ok(canvas.width * canvas.height === result.length / 4, "length of data: " + result.length / 4);
+
+    // cleanup 
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+test("Calculating pixel values ", function() {
+    "use strict";
+    
+    var ascObj = new Asciizer();
+
+    ascObj.loadImage(testImages.diag);
+    ascObj.setCanvasSize(20);
+    ascObj.draw();
+    ascObj.readCanvas();
+    ascObj.calculate();
+
+    ok(ascObj.valueArray.length === ascObj.pixelLightness.length * 4, "correct array length");
+	console.log(ascObj);
 });
