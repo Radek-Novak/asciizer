8bd4d4d45f6578f57a0394cee8d811b3bf93ed4a
Radek Novak <radek.novak.cze@gmail.com>
2014-05-28 22:21:11 +0200

    drawing

--- build/js/main.js
+++ build/js/main.js
@@ -183,28 +183,27 @@ function Drawing (sel) {
 	var $drawing = $(sel),
 		mouseMoveHandle;
 
+	function hovChar (c) {
+		return '<span class="hovered">' + c + '</span>';
+	}
+	function drawChar (c) {
+		return '<span class="drawn">' + c + '</span>';
+	}
 	var getPos = function (event) {
 		
 		var $this = $(this),
 			parentOffset = $this.parent().offset(),
         	relativeXPosition = (event.pageX - parentOffset.left),
-        	//relativeYPosition = (event.pageY - parentOffset.top),
-			//lineNumber = $('.box__subbox--drawing pre').index($this),
-			charIndex = Math.floor((relativeXPosition / 8) - 1),
+			charIndex = Math.floor((relativeXPosition / 8)),
 			charLine = $this.text(),
 			charContent = charLine[charIndex],
-			newCharLine = charLine.substring(0, charIndex - 1) + '<span class="hovered">' + charContent + '</span>' + charLine.substring(charIndex);
+			newCharLine = charLine.substring(0, charIndex-1) + '*' + charLine.substring(charIndex);
 
-		//console.log($this.html());
-		//console.log(newCharLine);
-		//console.log(newCharLine);
-		//console.log($hovered.text(charContent).html());
-		//console.log(charContent, relativeXPosition, charIndex);
-		//return pos;
 		$this.html(newCharLine);
 	};
 
-	mouseMoveHandle = $drawing.mousemove(getPos);
+	//mouseMoveHandle = $drawing.mousemove(getPos);
+	mouseDownHandle = $drawing.mousedown(getPos);
 }
 
 drawing = Drawing('pre');
