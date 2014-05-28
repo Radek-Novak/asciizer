9c63cc3ff78c060082bec7932a79b6ea238282a5
Radek Novak <radek.novak.cze@gmail.com>
2014-05-28 08:01:33 +0200

    nicely broken drawing

--- build/js/main.js
+++ build/js/main.js
@@ -186,16 +186,25 @@ function Drawing (sel) {
 	var getPos = function (event) {
 		
 		var $this = $(this),
-			//lineIndex =  Math.floor((event.pageX) / 8),
-			charIndex = $('.box__subbox--drawing pre').index($this),
-			charContent = $this.text()[charIndex];
+			parentOffset = $this.parent().offset(),
+        	relativeXPosition = (event.pageX - parentOffset.left),
+        	//relativeYPosition = (event.pageY - parentOffset.top),
+			//lineNumber = $('.box__subbox--drawing pre').index($this),
+			charIndex = Math.floor((relativeXPosition / 8) - 1),
+			charLine = $this.text(),
+			charContent = charLine[charIndex],
+			newCharLine = charLine.substring(0, charIndex - 1) + '<span class="hovered">' + charContent + '</span>' + charLine.substring(charIndex);
 
-		console.log(charContent,charIndex);
+		//console.log($this.html());
+		//console.log(newCharLine);
+		//console.log(newCharLine);
+		//console.log($hovered.text(charContent).html());
+		//console.log(charContent, relativeXPosition, charIndex);
 		//return pos;
+		$this.html(newCharLine);
 	};
-	
-	mouseMoveHandle = $drawing.mousemove(getPos);
 
+	mouseMoveHandle = $drawing.mousemove(getPos);
 }
 
 drawing = Drawing('pre');
