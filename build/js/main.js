2e1db452ff5eaeeeb8c408d5554526d3b42d054a
Radek Novak <radek.novak.cze@gmail.com>
2014-05-29 20:15:52 +0200

    drawing object finished

--- build/js/main.js
+++ build/js/main.js
@@ -179,34 +179,66 @@ Array.prototype.remove = function(from, to) {
   this.length = from < 0 ? this.length + from : from;
   return this.push.apply(this, rest);
 };
-function Drawing (sel) {
-	var $drawing = $(sel),
-		mouseMoveHandle;
+function Drawing (sel, paramChar) {
+	"use strict";
 
-	function hovChar (c) {
-		return '<span class="hovered">' + c + '</span>';
-	}
-	function drawChar (c) {
-		return '<span class="drawn">' + c + '</span>';
-	}
-	var getPos = function (event) {
-		
-		var $this = $(this),
-			parentOffset = $this.parent().offset(),
-        	relativeXPosition = (event.pageX - parentOffset.left),
-			charIndex = Math.floor((relativeXPosition / 8)),
-			charLine = $this.text(),
-			charContent = charLine[charIndex],
-			newCharLine = charLine.substring(0, charIndex-1) + '*' + charLine.substring(charIndex);
+	var $drawing = $(sel).find('pre'),
 
-		$this.html(newCharLine);
+		mouseMoveHandle, 
+		mouseDownHandle,
+		mouseClickHandle,
+
+		char,
+
+		drawChar = 
+			function drawChar ($this, chr, mouseDownCond) {
+				return function () {
+					if (mouseDownCond || mouseDown) {
+						var $this = $(this),
+							parentOffset = $this.parent().offset(),
+				        	relativeXPosition = (event.pageX - parentOffset.left),
+							charIndex = Math.floor((relativeXPosition / 8)),
+							charLine = $this.text(),
+							charContent = charLine[charIndex],
+							newCharLine = charLine.substring(0, charIndex-1) + chr + charLine.substring(charIndex);
+
+						$this.html(newCharLine);
+					}
+				};
+			};
+
+	this.attachHandles = function () {
+		mouseMoveHandle = null;
+		mouseDownHandle = null;
+		mouseClickHandle = null;
+		mouseMoveHandle = $drawing.mousemove(drawChar(this, char));
+		mouseDownHandle = $drawing.mouseover(drawChar(this, char));
+		mouseClickHandle = $drawing.click(drawChar(this, char, true));
 	};
 
-	//mouseMoveHandle = $drawing.mousemove(getPos);
-	mouseDownHandle = $drawing.mousedown(getPos);
+	this.changeChar = function (c) {
+		char = c;
+		this.attachHandles();
+	};
+
+	// init 
+	this.changeChar(paramChar);
+
 }
 
-drawing = Drawing('pre');
+var mouseDown = false,
+	docbody = document.body;
+
+docbody.onmousedown = function() { 
+  mouseDown = true;
+};
+
+docbody.onmouseup = docbody.onmouseleave =function() {
+  mouseDown = false;
+};
+
+
+drawing = new Drawing('.box__subbox--drawing', '*');
 
 var DropHandler = {
 	$dropzone: null,
