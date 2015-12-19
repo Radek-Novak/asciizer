5d92cc5119ae96644ec899c846dd743882e6a368
Radek Novak <radek.novak.cze@gmail.com>
2015-12-19 22:07:49 +0100

    Named arguments

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // #!/usr/bin/env node
 
 var argv = require('yargs').argv
-var asciize = require('./js/asciizer.js').asciize
+var asciize = require('./js/asciizer.js')
 
-asciize(argv._[0], argv._[1], argv._[2], rows => console.log(rows.join('\n')));
+asciize(argv._[0], argv.w, argv.h, rows => console.log(rows.join('\n')));
